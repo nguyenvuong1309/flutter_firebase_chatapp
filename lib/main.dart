9221028a4e6f427c00490c80fd0477af2fc7ba83
Nguyen-Duc-Vuong <vuong.nd@vgcorp.vn>
2024-07-29 07:19:47 +0700

    chore: Add necessary project files and configurations

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_project/Login%20Signup/Screen/home_screen.dart';
+import 'Real Time Database/query_in_realtime.dart';
+
+Future<void> main() async {
+  // Initialize Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  // Run your app
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // home: RealTimeDatabaseQueary(),
+        home: HomeScreen());
+  }
+}
+// Before start the video setup your project with firebase
+// If you have any problem to setup then firebase setup video is in i button and description
